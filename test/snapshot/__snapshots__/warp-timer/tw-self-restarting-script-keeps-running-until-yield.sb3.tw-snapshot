qxsck/scratch-vm-1

commit f548d1b33c5a7dfa9053586e921384723f6cb407

Fix stopping a thread from inside of its blocks
Works in command or reporter after yielding or not.
These snippets are covered by the tests:
thread.status = Thread.STATUS_DONE;
thread.status = 4; (indirectly)
sequencer.retireThread(thread);

src/compiler/jsexecute.js
@@ -163,7 +163,7 @@ const executeInCompatibilityLayer = function*(inputs, blockFunction, isWarp, use
         return returnValue;
     }
 
-    if (thread.status === 1 /* STATUS_PROMISE_WAIT */) {
+    if (thread.status === 1 /* STATUS_PROMISE_WAIT */ || thread.status === 4 /* STATUS_DONE */) {
         // Something external is forcing us to stop
         yield;
         // Make up a return value because whatever is forcing us to stop can't specify one
@@ -190,13 +190,11 @@ const executeInCompatibilityLayer = function*(inputs, blockFunction, isWarp, use
             return returnValue;
         }
 
-        if (thread.status === 1 /* STATUS_PROMISE_WAIT */) {
+        if (thread.status === 1 /* STATUS_PROMISE_WAIT */ || thread.status === 4 /* STATUS_DONE */) {
             yield;
             return finish('');
         }
     }
-
-    // todo: do we have to do anything extra if status is STATUS_DONE?
 
     return finish(returnValue);
 }`;

src/engine/execute.js
@@ -579,6 +579,9 @@ const execute = function (sequencer, thread) {
                     parentValues[inputName] = primitiveReportedValue;
                 }
             }
+        } else if (thread.status === Thread.STATUS_DONE) {
+            // Nothing else to execute.
+            break;
         }
     }
 

src/engine/sequencer.js
@@ -237,6 +237,9 @@ class Sequencer {
             } else if (thread.status === Thread.STATUS_YIELD_TICK) {
                 // stepThreads will reset the thread to Thread.STATUS_RUNNING
                 return;
+            } else if (thread.status === Thread.STATUS_DONE) {
+                // Nothing more to execute.
+                return;
             }
             // If no control flow has happened, switch to next block.
             if (thread.peekStack() === currentBlockId && !thread.peekStackFrame().waitingReporter) {

test/integration/tw-block-stop-thread.js
@@ -1,0 +1,198 @@
+const {test} = require('tap');
+const fs = require('fs');
+const path = require('path');
+const VM = require('../../src/virtual-machine');
+const Thread = require('../../src/engine/thread');
+const Clone = require('../../src/util/clone');
+
+const fixturePath = path.join(__dirname, '../fixtures/tw-block-stop-thread.sb3');
+const fixtureData = fs.readFileSync(fixturePath);
+
+const stopByRetireThread = thread => {
+    thread.target.runtime.sequencer.retireThread(thread);
+};
+
+const stopBySetStatus = thread => {
+    thread.status = Thread.STATUS_DONE;
+};
+
+test('constants', t => {
+    t.equal(Thread.STATUS_DONE, 4);
+    t.end();
+});
+
+for (const [enableCompiler, stopFunction] of [
+    [true, stopByRetireThread],
+    [true, stopBySetStatus],
+    [false, stopByRetireThread],
+    [false, stopBySetStatus]
+]) {
+    const subtestName = `${enableCompiler ? 'compiler' : 'interpreter'} - ${stopFunction.name}`;
+
+    test(`${subtestName} - stop in command block`, t => {
+        const vm = new VM();
+        vm.setCompilerOptions({enabled: enableCompiler});
+        t.equal(vm.runtime.compilerOptions.enabled, enableCompiler);
+
+        const callOrder = [];
+        vm.addAddonBlock({
+            procedureCode: 'first block %s',
+            arguments: ['number or text'],
+            callback: (args, util) => {
+                callOrder.push(['first block', Clone.simple(args)]);
+                stopFunction(util.thread);
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'inner block',
+            return: 1,
+            callback: args => {
+                callOrder.push(['input block', Clone.simple(args)]);
+                return callOrder.length;
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'second block',
+            callback: args => {
+                callOrder.push(['second block', Clone.simple(args)]);
+            }
+        });
+
+        vm.loadProject(fixtureData).then(() => {
+            vm.greenFlag();
+            vm.runtime._step();
+            t.same(callOrder, [
+                ['input block', {}],
+                ['first block', {'number or text': 1}]
+            ]);
+            t.end();
+        });
+    });
+
+    test(`${subtestName} - stop in command block after yielding once`, t => {
+        const vm = new VM();
+        vm.setCompilerOptions({enabled: enableCompiler});
+        t.equal(vm.runtime.compilerOptions.enabled, enableCompiler);
+
+        const callOrder = [];
+        vm.addAddonBlock({
+            procedureCode: 'first block %s',
+            arguments: ['number or text'],
+            callback: (args, util) => {
+                callOrder.push(['first block', Clone.simple(args)]);
+                if (callOrder.length === 1) {
+                    util.yield();
+                } else {
+                    stopFunction(util.thread);
+                }
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'inner block',
+            return: 1,
+            // Ignore calls because interpreter will re-evaluate on yield but compiler won't
+            callback: () => 5
+        });
+        vm.addAddonBlock({
+            procedureCode: 'second block',
+            callback: args => {
+                callOrder.push(['second block', Clone.simple(args)]);
+            }
+        });
+
+        vm.loadProject(fixtureData).then(() => {
+            vm.greenFlag();
+            vm.runtime._step();
+            t.same(callOrder, [
+                ['first block', {'number or text': 5}],
+                ['first block', {'number or text': 5}]
+            ]);
+            t.end();
+        });
+    });
+
+    test(`${subtestName} - stop in reporter block`, t => {
+        const vm = new VM();
+        vm.setCompilerOptions({enabled: enableCompiler});
+        t.equal(vm.runtime.compilerOptions.enabled, enableCompiler);
+
+        const callOrder = [];
+        vm.addAddonBlock({
+            procedureCode: 'first block %s',
+            arguments: ['number or text'],
+            callback: args => {
+                callOrder.push(['first block', Clone.simple(args)]);
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'inner block',
+            return: 1,
+            callback: (args, util) => {
+                callOrder.push(['input block', Clone.simple(args)]);
+                stopFunction(util.thread);
+                return callOrder.length;
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'second block',
+            callback: args => {
+                callOrder.push(['second block', Clone.simple(args)]);
+            }
+        });
+
+        vm.loadProject(fixtureData).then(() => {
+            vm.greenFlag();
+            vm.runtime._step();
+            t.same(callOrder, [
+                ['input block', {}]
+            ]);
+            t.end();
+        });
+    });
+
+    test(`${subtestName} - stop in reporter block after yielding once`, t => {
+        const vm = new VM();
+        vm.setCompilerOptions({enabled: enableCompiler});
+        t.equal(vm.runtime.compilerOptions.enabled, enableCompiler);
+
+        const callOrder = [];
+        vm.addAddonBlock({
+            procedureCode: 'first block %s',
+            arguments: ['number or text'],
+            callback: args => {
+                callOrder.push(['first block', Clone.simple(args)]);
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'inner block',
+            return: 1,
+            callback: (args, util) => {
+                callOrder.push(['input block', Clone.simple(args)]);
+                if (callOrder.length === 1) {
+                    util.yield();
+                    // TODO: interpreter bug, should not need to do this...
+                    util.thread.peekStackFrame().waitingReporter = true;
+                } else {
+                    stopFunction(util.thread);
+                }
+                return callOrder.length;
+            }
+        });
+        vm.addAddonBlock({
+            procedureCode: 'second block',
+            callback: args => {
+                callOrder.push(['second block', Clone.simple(args)]);
+            }
+        });
+
+        vm.loadProject(fixtureData).then(() => {
+            vm.greenFlag();
+            vm.runtime._step();
+            t.same(callOrder, [
+                ['input block', {}],
+                ['input block', {}]
+            ]);
+            t.end();
+        });
+    });
+}

commit 6ce931b60a21693ef35a33827d572a70f665ce9f

Add test for behavior of self-restarting scripts
The behavior is non-trivial as it will keep running the old thread
unti it yields.

I briefly tested putting `thread.status = 4` at the start of _restartThread
and all the old execution tests still passed, so let's just add a bit more
coverage. This was nearly regressed by f548d1b33c5a7dfa9053586e921384723f6cb407.

test/snapshot/__snapshots__/tw-self-restarting-script-keeps-running-until-yield.sb3.tw-snapshot
@@ -1,0 +1,38 @@
+// TW Snapshot
+// Input SHA-256: dc029abbf128cea0830f4578e89619949837ec26dfa76f110ca599a9ee12e66e
+
+// Sprite1 script
+(function factoryXYZ(thread) { const target = thread.target; const runtime = target.runtime; const stage = runtime.getTargetForStage();
+const b0 = runtime.getOpcodeFunction("looks_say");
+const b1 = stage.variables["G{NQ*NXG*qzywe1Q3?FM"];
+const b2 = stage.variables["`jEk@4|i[#Fk?(8x)AV.-my variable"];
+return function* genXYZ () {
+yield* executeInCompatibilityLayer({"MESSAGE":"plan 1",}, b0, false, false, "c", null);
+b1.value = 0;
+b2.value = 0;
+startHats("event_whenbroadcastreceived", { BROADCAST_OPTION: "message1" });
+retire(); return;
+}; })
+
+// Sprite1 script
+(function factoryXYZ(thread) { const target = thread.target; const runtime = target.runtime; const stage = runtime.getTargetForStage();
+const b0 = stage.variables["`jEk@4|i[#Fk?(8x)AV.-my variable"];
+const b1 = stage.variables["G{NQ*NXG*qzywe1Q3?FM"];
+const b2 = runtime.getOpcodeFunction("looks_say");
+return function* genXYZ () {
+if (compareEqual(b0.value, 0)) {
+b0.value = 1;
+startHats("event_whenbroadcastreceived", { BROADCAST_OPTION: "message1" });
+b1.value = 1;
+for (var a0 = 1; a0 >= 0.5; a0--) {
+yield;
+}
+b1.value = 2;
+} else {
+if (((+b1.value || 0) === 1)) {
+yield* executeInCompatibilityLayer({"MESSAGE":"pass",}, b2, false, false, "p", null);
+}
+yield* executeInCompatibilityLayer({"MESSAGE":"end",}, b2, false, false, "n", null);
+}
+retire(); return;
+}; })

test/snapshot/__snapshots__/warp-timer/tw-self-restarting-script-keeps-running-until-yield.sb3.tw-snapshot
@@ -1,0 +1,38 @@
+// TW Snapshot
+// Input SHA-256: dc029abbf128cea0830f4578e89619949837ec26dfa76f110ca599a9ee12e66e
+
+// Sprite1 script
+(function factoryXYZ(thread) { const target = thread.target; const runtime = target.runtime; const stage = runtime.getTargetForStage();
+const b0 = runtime.getOpcodeFunction("looks_say");
+const b1 = stage.variables["G{NQ*NXG*qzywe1Q3?FM"];
+const b2 = stage.variables["`jEk@4|i[#Fk?(8x)AV.-my variable"];
+return function* genXYZ () {
+yield* executeInCompatibilityLayer({"MESSAGE":"plan 1",}, b0, false, false, "c", null);
+b1.value = 0;
+b2.value = 0;
+startHats("event_whenbroadcastreceived", { BROADCAST_OPTION: "message1" });
+retire(); return;
+}; })
+
+// Sprite1 script
+(function factoryXYZ(thread) { const target = thread.target; const runtime = target.runtime; const stage = runtime.getTargetForStage();
+const b0 = stage.variables["`jEk@4|i[#Fk?(8x)AV.-my variable"];
+const b1 = stage.variables["G{NQ*NXG*qzywe1Q3?FM"];
+const b2 = runtime.getOpcodeFunction("looks_say");
+return function* genXYZ () {
+if (compareEqual(b0.value, 0)) {
+b0.value = 1;
+startHats("event_whenbroadcastreceived", { BROADCAST_OPTION: "message1" });
+b1.value = 1;
+for (var a0 = 1; a0 >= 0.5; a0--) {
+yield;
+}
+b1.value = 2;
+} else {
+if (((+b1.value || 0) === 1)) {
+yield* executeInCompatibilityLayer({"MESSAGE":"pass",}, b2, false, false, "p", null);
+}
+yield* executeInCompatibilityLayer({"MESSAGE":"end",}, b2, false, false, "n", null);
+}
+retire(); return;
+}; })